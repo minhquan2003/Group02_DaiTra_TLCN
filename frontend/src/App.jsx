5747b7ea6eb910571e8746c80dcb3212730a2431
minhquan2003 <minhquan31102003@gmail.com>
2024-10-02 21:41:07 +0700

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className='bg-red-400 text-white'>Demo</div>
+  )
+}
+
+export default App
