d695eb98a302a5003dad8728c181a29f337e518a
PKTam <tampham11072003@gmail.com>
2024-10-28 22:54:58 +0700

    front-end_authform

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content:[
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        primary: "#000000",
+        secondary: "#DB4444",
+      },
+    },
   },
   plugins: [],
-}
-
+};
